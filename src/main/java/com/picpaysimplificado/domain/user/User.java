4568df39b5ca3180cf377baf5870de6ab6e077bc
RodrigoBritoGit <Rodrigodossantosbrito@hotmail.com>
2024-06-13 13:18:27 -0300

    primeira versão

---
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,114 @@
+package com.picpaysimplificado.domain.user;
+
+import com.picpaysimplificado.dtos.UserDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name="users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique=true)
+    private String document;
+    @Column(unique=true)
+    private String email;
+    
+    
+    private BigDecimal balance;
+    
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDocument() {
+		return document;
+	}
+
+	public void setDocument(String document) {
+		this.document = document;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+    // Construtor e outros métodos da classe User
+
+    public BigDecimal getBalance() {
+        return balance != null ? balance : BigDecimal.ZERO;
+    }
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+	public UserType getUserType() {
+		return userType;
+	}
+
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+
+	private String password;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.document = data.document();
+        this.email = data.email();
+    }
+
+	public User(long l, String string, String string2, String string3, String string4, String string5,
+			BigDecimal bigDecimal, UserType common) {
+		// TODO Auto-generated constructor stub
+	}
+}
